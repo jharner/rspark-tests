3dc4115bd25014745b2c8e95967e850dc4004f45
jharner <jharner@me.com>
2020-09-07 16:31:26 +0000

    corrected rhdfs test file

--- rhdfs-test.R
+++ rhdfs-test.R
@@ -1,13 +1,18 @@
-#!/bin/bash
+options(warn=-1)
+
+Sys.getenv(c("HADOOP_HOME", "HADOOP_CMD"))
+
+# load rhdfs library and initialize rhdfs
+library(rhdfs)
+hdfs.init()
 
 # retrieve rhdfs defaults
-hdfs.defaults()
+hdfs.defaults("conf")
 
 # make a directory and copy a file from the local filesystem to hdfs using rhdfs
-library(rhdfs)
 hdfs.mkdir("temp")
-hdfs.put("cdat.csv", "/temp")
-hdfs.ls("temp", recurse = TRUE)
+hdfs.put("cdat.csv", "temp")
+hdfs.ls("temp")
 
 # remove the file and directory from hdfs
 hdfs.rm("temp/cdat.csv")
