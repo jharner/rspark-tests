7e67464cc826bf539aaf3f00b6f22dc0f51df8d2
jharner <ejharner@gmail.com>
2020-09-12 05:49:48 +0000

    added spark_apply test

---
+++ spark_apply-test.R
@@ -0,0 +1,11 @@
+library(dplyr, warn.conflicts = FALSE)
+library(sparklyr)
+
+# start the sparklyr session
+master <- "local"
+# master <- "spark://master:7077"
+sc <- spark_connect(master, spark_home = Sys.getenv("SPARK_HOME"),
+                    method = c("shell"), app_name = "sparklyr")
+
+sdf_len(sc, length = 5, repartition = 1) %>%
+  spark_apply(function(e) I(e))
