e996a1e7265327a120029398ed2ebe6c223c888f
jharner <ejharber@gmail.com>
2020-09-20 04:15:29 +0000

    corrected directory path

--- rhdfs-test.R
+++ rhdfs-test.R
@@ -1,7 +1,6 @@
 options(warn=-1)
 
 Sys.getenv(c("HADOOP_HOME", "HADOOP_CMD"))
-setwd("/home/rstudio/rspark-tests")
 
 # load rhdfs library and initialize rhdfs
 library(rhdfs)
@@ -12,7 +11,7 @@ hdfs.defaults("conf")
 
 # make a directory and copy a file from the local filesystem to hdfs using rhdfs
 hdfs.mkdir("temp")
-hdfs.put("cdat.csv", "temp")
+hdfs.put("/home/rstudio/rspark-tests/cdat.csv", "temp")
 hdfs.ls("temp")
 
 # remove the file and directory from hdfs
